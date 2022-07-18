27c54e3a8e74171cf2fc85b6c2d96aae1e343d26
MD <dhmedy@googlemail.com>
2022-07-18 14:17:38 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+
+function intro3(name, stadt, alter) {
+
+    console.log('Hallo, mein Name ist ' + name + '. Ich bin ' + alter + ' Jahre alt. Ich komme aus ' + stadt);
+
+}
+
+intro3('SuperCoder', 27, 'Düsseldorf');
